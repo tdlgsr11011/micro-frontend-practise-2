eef37144ae36207f463ee5cb0f3feadd6be2cea9
tdlgsr11011 <atharvjoshi27@gmail.com>
2025-01-19 12:41:51 +0530

    adding header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,44 +1,16 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
-import Typography from "@material-ui/core/Typography";
-import { Button, ButtonGroup } from "@material-ui/core";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 
 const App = () => {
-  const openLinkedin = () => {
-    window.open("https://www.linkedin.com/in/atharvjoshi2705/");
-  };
-
-  const openGitRepo = () => {
-    window.open("https://github.com/tdlgsr11011/micro-frontend-practise-2");
-  };
   return (
-    <div>
-      <Typography
-        component="h1"
-        variant="h3"
-        align="center"
-        color="textPrimary"
-        gutterBottom
-      >
-        React Micro Frontend App
-      </Typography>
-      <Typography align="center">
-        <ButtonGroup
-          variant="contained"
-          color="primary"
-          aria-label="contained primary button group"
-        >
-          <Button variant="contained" color="primary" onClick={openLinkedin}>
-            Linkedin Profile
-          </Button>
-          <Button variant="outlined" color="primary" onClick={openGitRepo}>
-            Github repository
-          </Button>
-        </ButtonGroup>
-      </Typography>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
 
