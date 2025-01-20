9a6310b12fd2a1020a32d39095e502e62d413cb2
tdlgsr11011 <atharvjoshi27@gmail.com>
2025-01-20 11:05:02 +0530

    using memory history instead of browser history

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory } from "history";
 
 const mount = (el) => {
-  ReactDOM.render(<App />, el);
+  const history = createMemoryHistory();
+  ReactDOM.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === "development") {
