c743eea1c5bc8987dc0c1a3812c1d19365b6a820
tdlgsr11011 <atharvjoshi27@gmail.com>
2025-01-20 12:08:14 +0530

    making it work for marketing in isolation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -5,15 +5,17 @@ import { createMemoryHistory } from "history";
 
 const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
-  history.listen(onNavigate);
-  
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
   ReactDOM.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
