db4d7a9bb40f3446d5ff65b6e07648b0c0952ea0
tdlgsr11011 <atharvjoshi27@gmail.com>
2025-01-22 11:45:12 +0530

    implementing lazy loading

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import {
   StylesProvider,
   createGenerateClassName,
@@ -6,8 +6,9 @@ import {
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
-import MarketingApp from "./components/MarketingApp";
-import AuthApp from "./components/AuthApp";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -19,10 +20,12 @@ const App = () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
