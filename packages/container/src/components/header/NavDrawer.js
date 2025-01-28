67dee836398b70bea400f59fb5cb504b1536670c
tdlgsr11011 <atharvjoshi27@gmail.com>
2025-01-28 19:00:02 +0530

    making drawer items bigger

--- packages/container/src/components/header/NavDrawer.js
+++ packages/container/src/components/header/NavDrawer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Drawer } from "@material-ui/core";
+import styles from "./Header.module.scss";
 
 const NavDrawer = (props) => {
   return (
     <Drawer anchor={"right"} open={props.showDrawer} onClose={props.onClose}>
-      <div>Item1</div>
-      <div>Item1</div>
-      <div>Item1</div>
+      <div className={styles.drawerItem}>Item1</div>
+      <div className={styles.drawerItem}>Item1</div>
+      <div className={styles.drawerItem}>Item1</div>
     </Drawer>
   );
 };
