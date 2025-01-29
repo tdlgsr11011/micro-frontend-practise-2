a25cd75ae95570dd57ef94156e6e1a82cab6f78a
tdlgsr11011 <atharvjoshi27@gmail.com>
2025-01-29 19:20:35 +0530

    little modifications

--- packages/container/src/components/home/HomePage.js
+++ packages/container/src/components/home/HomePage.js
@@ -2,8 +2,9 @@ import React from "react";
 
 import myImage from "../../assets/images/20241108_125213_2.jpg";
 import styles from "./HomePage.module.scss";
+import WelcomeMessage from "./WelcomeMessage";
 
-const HomePage = (props) => {
+const HomePage = () => {
   return (
     <div className={styles.homePageContainer}>
       <div className={styles.profileContainer}>
@@ -11,21 +12,7 @@ const HomePage = (props) => {
           <img src={myImage} />
           <div>Atharv Joshi</div>
         </div>
-        <div className={styles.welcomeMessage}>
-          <div>
-            Hi, thanks for taking a minute to ckeck my resume, and more
-            importantly, this app!
-          </div>
-          <div>
-            I am just making an attempt to flex my skills here. To do so, I have
-            built this app made of several smaller Microfrontends which are
-            linked in the navbar at the top.
-          </div>
-          <div>
-            Do check out the github repository as well if you'd like to go
-            through the code.
-          </div>
-        </div>
+        <WelcomeMessage />
       </div>
     </div>
   );
