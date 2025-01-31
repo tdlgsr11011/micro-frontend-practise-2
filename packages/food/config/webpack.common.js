aa47923a0f678a29fea09fa62ec030bf89e89df1
tdlgsr11011 <atharvjoshi27@gmail.com>
2025-01-31 15:41:41 +0530

    setting up scss loaders in config

--- packages/food/config/webpack.common.js
+++ packages/food/config/webpack.common.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -12,6 +14,39 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.module\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // Extracts CSS into separate files
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]__[hash:base64:5]", // Enables CSS Modules
+              },
+            },
+          },
+          "sass-loader", // Compiles Sass to CSS
+        ],
+      },
+      {
+        test: /\.scss$/, // Handles global SCSS files
+        exclude: /\.module\.scss$/, // Exclude CSS Modules
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader", // No modules option here for global styles
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource", // Webpack 5 built-in loader
+      },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css", // Output file name for extracted CSS
+    }),
+  ],
 };
