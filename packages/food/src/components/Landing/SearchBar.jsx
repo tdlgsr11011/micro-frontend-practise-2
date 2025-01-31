9bda2cd81b4370dc0faa9c4e8456f94996bd68bf
tdlgsr11011 <atharvjoshi27@gmail.com>
2025-01-31 16:28:14 +0530

    searchbar basic gist

--- packages/food/src/components/Landing/SearchBar.jsx
+++ packages/food/src/components/Landing/SearchBar.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import styles from "./SearchBar.module.scss";
 
 const SearchBar = () => {
-  return <input type="text" className={styles.searchbar} />;
+  return (
+    <div className={styles.searchbarContainer}>
+      <input
+        type="text"
+        className={styles.searchbar}
+        placeholder="srarch a recipe eg. pizza"
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
