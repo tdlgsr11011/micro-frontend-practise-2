ebe957913eb47321f27fb0cc3a8d83ae858c698d
tdlgsr11011 <atharvjoshi27@gmail.com>
2025-01-31 12:43:51 +0530

    updating webpack config

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -18,7 +18,8 @@ const devConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        // marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        food: "food@http://localhost:8081/remoteEntry.js",
         auth: "auth@http://localhost:8082/remoteEntry.js",
       },
       shared: jsonPackage.dependencies,
