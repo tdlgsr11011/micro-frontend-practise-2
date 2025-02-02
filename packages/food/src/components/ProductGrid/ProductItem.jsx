2cde42c31e1efc4aa51bd54b4278d6907bf39c62
tdlgsr11011 <atharvjoshi27@gmail.com>
2025-02-02 22:59:31 +0530

    styling for product item

--- packages/food/src/components/ProductGrid/ProductItem.jsx
+++ packages/food/src/components/ProductGrid/ProductItem.jsx
@@ -3,7 +3,21 @@ import styles from "./ProductItem.module.scss";
 
 const ProductItem = (props) => {
   const { itemDetails } = props;
-  return <div className={styles.productItem}>Item</div>;
+  const title =
+    itemDetails.title?.length > 20
+      ? itemDetails.title.slice(0, 20) + "..."
+      : itemDetails.title;
+
+  return (
+    <div className={styles.productItem}>
+      <div className={styles.productImageContainer}>
+        <img src={itemDetails.image_url} />
+      </div>
+      <div>
+        <div>{title || "No title"}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductItem;
