f81a0adc6e8236b3a8796470108a55c4594c08bc
tdlgsr11011 <atharvjoshi27@gmail.com>
2025-02-02 15:28:27 +0530

    configuring recipestore to work with dev tools

--- packages/food/src/zustand/recipeStore.js
+++ packages/food/src/zustand/recipeStore.js
@@ -1,22 +1,28 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 const apiKey = process.env.FORKIFY_API_KEY;
 const baseUrl = process.env.FORKIFY_BASE_URL;
 
-const useRecipeStore = create((set) => ({
-  loading: false,
-  recipes: [],
+const useRecipeStore = create(
+  devtools(
+    (set) => ({
+      loading: false,
+      recipes: [],
 
-  fetchRecipes: async (queryText) => {
-    const url = `${baseUrl}/api/v2/recipes?search=${queryText}&key=${apiKey}`;
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      set((state) => ({ recipes: data.data.recipes }));
-    } catch (err) {
-      throw new Error(err);
-    }
-  },
-}));
+      fetchRecipes: async (queryText) => {
+        const url = `${baseUrl}/api/v2/recipes?search=${queryText}&key=${apiKey}`;
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+          set((state) => ({ recipes: data.data.recipes }));
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    }),
+    { name: "recipeStore" }
+  )
+);
 
 export default useRecipeStore;
